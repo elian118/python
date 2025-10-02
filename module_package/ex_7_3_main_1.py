74837b6b76f14bf2d16a3795ba21275c2f24729a
elian118 <elian118@naver.com>
2025-10-02 09:01:03 +0900

    update ex_7_3_main_1.py

--- module_package/ex_7_3_main_1.py
+++ module_package/ex_7_3_main_1.py
@@ -4,7 +4,8 @@ print(module_a.variable_a)
 print(module_b.variable_b)
 
 # __init__ → 특정 폴더에 위치한 모든 모듈 모음(패키지)의 정의를 통해 모듈을 가져와 사용하는 형태
-# from 패키지(폴더)명 import * 을 사용하려면 반드시 __init__.py 파일이 필요하다.
+# from 패키지(폴더)명 import * 사용 시 from 경로에 __init__.py 파일이 존재한다면 
+# __init__.py 설정에 따른 모듈만 가져올 수 있다.(즉, 있어도 되고 없어도 된다)
 # 패키지 폴더 안에 __init__.py 파일이 존재하면 import로 접근 시 먼저 __init__.py가 실행된다.
 
 # 참고 - 파일명.cpython.pyc 파일
